44c34d308ac91f9d364279e66e960b13b2246ecd
Asfand Khan <khanasfand174@gmail.com>
2022-05-24 16:40:44 +0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// initializing express
+const app = express();
+
+// config dotenv
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan("dev"));
