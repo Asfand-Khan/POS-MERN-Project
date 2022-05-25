4ae3e98d371e65bedee5052a1420c557d5d6e9db
Asfand Khan <khanasfand174@gmail.com>
2022-05-25 23:19:43 +0500

    Create itemsRoutes.js

---
+++ router/itemsRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { getAllItems, addItem } = require("../controllers/itemController");
+const router = express.Router();
+
+//router
+//method - get
+router.get("get-items", getAllItems);
+
+//method - post
+router.post("add-item", addItem);
