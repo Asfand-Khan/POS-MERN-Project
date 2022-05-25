e8ff4d9d09963e5434f9b6238b4ed0c996f07ff4
Asfand Khan <khanasfand174@gmail.com>
2022-05-25 23:34:10 +0500

    Update itemController.js

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const ItemModel = require("../models/itemModel");
 
-export const getAllItems = async (req, res) => {
+const getAllItems = async (req, res) => {
   try {
     const items = await ItemModel.find();
-    res.status(200).res(items);
+    res.status(200).send(items);
+    // console.log("first");
   } catch (error) {
     console.log(error);
   }
 };
-export const addItem = () => {};
+const addItem = () => {};
+
+module.exports = { getAllItems, addItem };
